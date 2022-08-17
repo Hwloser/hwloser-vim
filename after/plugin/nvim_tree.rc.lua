8c594cb969aca638189cac966f7365a0dd489b18
huanwei <hwloser@163.com>
2022-08-17 19:23:12 +0800

    调整nvimtree，便于使用

--- after/plugin/nvim_tree.rc.lua
+++ after/plugin/nvim_tree.rc.lua
@@ -3,16 +3,58 @@ if not status then return end
 
 tree.setup({
   sort_by = 'case_sensitive',
+  open_on_setup = true,
   view = {
     adaptive_size = true,
-    --    mappings = {
-    --      list = {
-    --        { key = 'u', action = 'dir_up' },
-    --      },
-    --    },
+    mappings = {
+      list = {
+        -- { key = 'p', action = 'dir_up' },
+      },
+    },
+    adaptive_size = true,
+    side = 'left',
+    signcolumn = 'yes',
+  },
+  filesystem_watchers = {
+    enable = true,
+    debounce_delay = 50
+  },
+  trash = {
+    cmd = "gio trash",
+    require_confirm = true,
   },
   renderer = {
     group_empty = true,
+    icons = {
+      git_placement = 'before',
+      show = {
+        git = true
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        bookmark = "",
+        folder = {
+          arrow_closed = "",
+          arrow_open = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
+      }
+    }
   },
   filters = {
     dotfiles = true
