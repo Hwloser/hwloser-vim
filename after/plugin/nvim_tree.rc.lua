9d50c4912c852f8369288e5dd04ea65606aa375e
huanwei <hwloser@163.com>
2022-08-17 19:40:58 +0800

    调整nvim tree作为最后一个退出的时候，进行的事件

--- after/plugin/nvim_tree.rc.lua
+++ after/plugin/nvim_tree.rc.lua
@@ -68,7 +68,8 @@ vim.api.nvim_create_autocmd("BufEnter", {
   nested = true,
   callback = function()
     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-      vim.cmd "quit"
+      -- TODO: to be optimized
+      vim.cmd "quitall!"
     end
   end
 })
