30ff05af6d90f826aa250a4513128a58044f9e7f
huanwei <hwloser@163.com>
2023-02-25 00:44:55 +0800

    Merge v2.0.0 into master and clear existing code

--- after/plugin/dockerls.rc.lua
+++
@@ -1,9 +0,0 @@
-local status, lsp = pcall(require, 'lspconfig')
-if not status then return end
-
-lsp.dockerls.setup {
-  -- default values
-  -- url: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-  -- cmd = { "docker-langserver", "--stdio" },
-
-}
--- after/plugin/markdown-preview.rc.lua
+++
@@ -1,8 +0,0 @@
--- Content from https://github.com/iamcco/markdown-preview.nvim
--- set to 1, nvim will open the preview window after entering the markdown buffer
-vim.g.mkdp_auto_start = 1
-
--- spefify browser to open preview page
-vim.g.mkdp_browser = '/Applications/Min.app/Contents/MacOS/Min'
-
-vim.keymap.set('n', '<space>md', ':MarkdownPreviewToggle<CR>', { silent = true })
