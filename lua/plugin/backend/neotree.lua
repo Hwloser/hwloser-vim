8cc0e9313dde9bca88d687b10e08c012636460ad
huanwei <hwloser@163.com>
2023-02-26 14:57:42 +0800

    [neovim] Hidden gitignored for neo-tree, avoid so much file listed in file system

--- lua/plugin/backend/neotree.lua
+++ lua/plugin/backend/neotree.lua
@@ -205,7 +205,7 @@ return {
         filtered_items = {
           visible = false, -- when true, they will just be displayed differently than normal items
           hide_dotfiles = true,
-          hide_gitignored = false,
+          hide_gitignored = true,
           hide_hidden = true, -- only works on Windows for hidden files/directories
           hide_by_name = {
             --"node_modules"
