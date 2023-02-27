72e66edd6ca2276539c1abb465fc4dec30610fc4
huanwei <hwloser@163.com>
2023-02-27 20:37:46 +0800

    [nevim] Add treesitter plugins

---
+++ lua/plugin/backend/treesitter.lua
@@ -0,0 +1,61 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  version = false,
+  build = ":TSUpdate",
+  event = {
+    "BufReadPost",
+    "BufNewFile",
+  },
+  dependencies = {
+    {
+          "nvim-treesitter/nvim-treesitter-textobjects",
+    init = function()
+      -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+      local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      local enabled = false
+      if opts.textobjects then
+        for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+          if opts.textobjects[mod] and opts.textobjects[mod].enable then
+            enabled = true
+            break
+          end
+        end
+      end
+      if not enabled then
+        require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+      end
+    end
+    },
+  },
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true, disable = { "python" } },
+    context_commentstring = { enable = true, enable_autocmd = false },
+    ensure_installed = {
+      "bash",
+      "help",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+      },
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end
+}
