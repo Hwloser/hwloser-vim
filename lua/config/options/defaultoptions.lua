d5af24ec36d281cbda9730530b2c66e5a165fb9d
huanwei <hwloser@163.com>
2023-02-27 20:51:25 +0800

    [neotree] change the default fold strategy

--- lua/config/options/defaultoptions.lua
+++ lua/config/options/defaultoptions.lua
@@ -13,7 +13,7 @@ return {
   },
   conceallevel = 0, -- so taht `` is visible in markdown files
   fileencoding = "utf-8", -- the file encoding
-  foldmethod = "expr", -- folding method, which will let treesitter decided, set `manual` for manual operattion
+  foldmethod = "manual", -- folding method, which will let treesitter decided, set `manual` for manual operattion
   foldexpr = "nvim_treesitter#foldexpr()", -- set empty will let you decided, there will let treesitter decided folding
   hlsearch = true, -- highlight all matches on previous search pattern
   ignorecase = true, -- ignore case in search patterns
--- lua/plugin/backend/neotree.lua
+++ lua/plugin/backend/neotree.lua
@@ -227,7 +227,7 @@ return {
         },
         follow_current_file = false, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
-        group_empty_dirs = false, -- when true, empty folders will be grouped together
+        group_empty_dirs = true, -- when true, empty folders will be grouped together
         hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
         -- in whatever position is specified in window.position
         -- "open_current",  -- netrw disabled, opening a directory opens within the
