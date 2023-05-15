13e35752831c331c2ef6b1c5bcb4b45382f76836
huanwei <hwloser@163.com>
2023-05-15 13:41:21 +0800

    [plugins][persistence] Add persistence.lua for session manager, we could auto detect windows layout and then restore after exit

---
+++ lua/plugin/backend/persistence.lua
@@ -0,0 +1,12 @@
+-- https://github.com/folke/persistence.nvim
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre",
+  opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+  -- stylua: ignore
+  keys = {
+    { "<leader>ps", function() require("persistence").load() end,                desc = "Restore Session" },
+    { "<leader>pl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+    { "<leader>pd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
+  },
+}
